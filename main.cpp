4229e167c050ca2b96590c646b8c7b0e58c26a3a
DiwakarKC <70564714+DiwakarKC@users.noreply.github.com>
2026-08-10 18:23:08 +0545

    Add files via upload
    Sequential Image processing of each frame, GIves HDR output

---
+++ main.cpp
@@ -0,0 +1,631 @@
+#include<iostream>
+#include<opencv2/opencv.hpp>
+#include<filesystem>
+#include <cstdlib>
+
+using namespace std;
+using namespace cv;
+// Using a name fs as filesystem
+namespace fs = filesystem;
+
+// Creating global structure variable for callback function
+struct Parameters
+{
+    Mat input;
+    Mat output;
+    Mat Clahe_output;
+    Mat Sig_output;
+    Mat final_output;
+    int cliplimit;
+    int st;
+    int c;
+    int val;
+    int level;
+
+};
+
+void gamma_lut(int& val,Mat& input,Mat& output)
+{
+    output = Mat(input.rows,input.cols,CV_8UC3);
+    int gam[256];
+    for(int i=0; i<256; i++)
+    {
+        gam[i] = 255 * pow(i/255.0,val/10.0);
+    }
+    for(int y=0; y<input.rows; y++)
+    {
+        for(int x=0; x<input.cols; x++)
+        {
+            Vec3b pixels = input.at<Vec3b>(y,x);
+            unsigned char B = pixels[0];
+            unsigned char G = pixels[1];
+            unsigned char R = pixels[2];
+
+            B = gam[B];
+            G = gam[G];
+            R = gam[R];
+
+            pixels[0] = B;
+            pixels[1] = G;
+            pixels[2] = R;
+            output.at<Vec3b>(y,x)= pixels;
+        }
+    }
+}
+void Sigmoid_TM(int& st,int& c,Mat& input,Mat& output)
+{
+    output = Mat::zeros(input.size(),input.type());
+    int Sig[256];
+    float Smin = (1.0/(1.0 + pow(2.71828,st*(c/10.0))));
+    float Smax = (1.0/(1.0 + pow(2.71828,-st*(1-(c/10.0)))));
+    float subs = Smax-Smin;
+    for(int i=0; i<256; i++)
+    {
+        float S = (1.0/(1.0 + pow(2.71828,-st * ((i/255.0) - (c/10.0)))));
+        float Snorm = (S-Smin)/subs;
+        Sig[i] = 255*Snorm;
+    }
+
+    for(int y=0; y<input.rows; y++)
+    {
+        for(int x=0; x<input.cols; x++)
+        {
+            Vec3b pixels = input.at<Vec3b>(y,x);
+            unsigned char B = pixels[0];
+            unsigned char G = pixels[1];
+            unsigned char R = pixels[2];
+
+            B = Sig[B];
+            G = Sig[G];
+            R = Sig[R];
+
+            pixels[0] = B;
+            pixels[1] = G;
+            pixels[2] = R;
+
+            output.at<Vec3b>(y,x) = pixels;
+        }
+    }
+}
+
+void WBC(Mat& input,Mat& output)
+{
+    output = Mat(input.rows,input.cols,CV_8UC3);
+    int Gx[3][3]= {{-1,0,1},{-2,0,2},{-1,0,1}};
+    int Gy[3][3]= {{-1,-2,-1},{0,0,0},{1,2,1}};
+    int GradX_R=0,GradX_G=0,GradX_B=0;
+    int GradY_R=0,GradY_G=0,GradY_B=0;
+    int MR=0,MG=0,MB=0;
+    int ER=0,EG=0,EB=0;
+    int gray=0;
+    float kR=0.0,kG=0.0,kB=0.0;
+    for(int y=1; y<input.rows-1; y++)
+    {
+        for(int x=1; x<input.cols-1; x++)
+        {
+            for(int i=-1; i<=1; i++)
+            {
+                for(int j=-1; j<=1; j++)
+                {
+                    Vec3b pixels = input.at<Vec3b>(y+i,x+j);
+                    unsigned char B = pixels[0];
+                    unsigned char G = pixels[1];
+                    unsigned char R = pixels[2];
+
+                    GradX_B = GradX_B + B * Gx[i+1][j+1];
+                    GradX_G = GradX_G + G * Gx[i+1][j+1];
+                    GradX_R = GradX_R + R * Gx[i+1][j+1];
+
+                    GradY_B = GradY_B + B * Gy[i+1][j+1];
+                    GradY_G = GradY_G + G * Gy[i+1][j+1];
+                    GradY_R = GradY_R + R * Gy[i+1][j+1];
+                }
+            }
+            MB = MB + sqrt(GradX_B*GradX_B+GradY_B*GradY_B);
+            MG = MG + sqrt(GradX_G*GradX_G+GradY_G*GradY_G);
+            MR = MR + sqrt(GradX_R*GradX_R+GradY_R*GradY_R);
+        }
+    }
+
+    EB = MB/((input.rows-2)*(input.cols-2));
+    EG = MG/((input.rows-2)*(input.cols-2));
+    ER = MR/((input.rows-2)*(input.cols-2));
+
+    gray = (EB+EG+ER)/3;
+    kR = (gray*1.0)/ER;
+    kG = (gray*1.0)/EG;
+    kB = (gray*1.0)/EB;
+
+//--------------------------------------------------------------------
+//Doing WBC on each channels
+    for(int y=0; y<input.rows; y++)
+    {
+        for(int x=0; x<input.cols; x++)
+        {
+            Vec3b pixels = input.at<Vec3b>(y,x);
+            unsigned char B = pixels[0];
+            unsigned char G = pixels[1];
+            unsigned char R = pixels[2];
+
+            B = kB * B;
+            G = kG * G;
+            R = kR * R;
+
+            B = (B>255)?255:B;
+            G = (G>255)?255:G;
+            R = (R>255)?255:R;
+
+            pixels[0] = B;
+            pixels[1] = G;
+            pixels[2] = R;
+
+            output.at<Vec3b>(y,x) = pixels;
+        }
+    }
+}
+void unmask_sharp(int& level,Mat& input,Mat& output)
+{
+    int mask[3][3] = {{1,2,1},{2,4,2},{1,2,1}};
+    int red=0,blue=0,green=0;
+    int r_o=0,g_o=0,b_o=0;
+    Vec3b pixels1;
+    output = Mat::zeros(input.size(),input.type());
+    for(int y=1; y<input.rows-1; y++)
+    {
+        for(int x=1; x<input.cols-1; x++)
+        {
+            for(int i=-1; i<=1; i++)
+            {
+                for(int j=-1; j<=1; j++)
+                {
+                    Vec3b pixels = input.at<Vec3b>(y+i,x+j);
+                    unsigned char B = pixels[0];
+                    unsigned char G = pixels[1];
+                    unsigned char R = pixels[2];
+
+                    blue = blue + B * mask[i+1][j+1];
+                    green = green + G * mask[i+1][j+1];
+                    red = red + R * mask[i+1][j+1];
+                }
+            }
+            Vec3b pixels = input.at<Vec3b>(y,x);
+            unsigned char B = pixels[0];
+            unsigned char G = pixels[1];
+            unsigned char R = pixels[2];
+
+            blue=blue/16;
+            green=green/16;
+            red=red/16;
+
+//            blue = max(0,min(255,blue));
+//            green = max(0,min(255,green));
+//            red = max(0,min(255,red));
+
+//                blue = (blue<0)?0:blue;
+//                blue = (blue>255)?255:blue;
+//
+//                 green = (green<0)?0:green;
+//                green = (green>255)?255:green;
+//
+//                red= (red<0)?0:red;
+//                red = (red>255)?255:red;
+//                 cout << blue <<endl;
+            b_o = B + ((B-blue)*level)/100;
+            g_o = G + ((G-green)*level)/100;
+            r_o = R + ((R-red)*level)/100;
+
+            pixels1[0] = max(0,min(255,b_o));
+            pixels1[1] = max(0,min(255,g_o));
+            pixels1[2] = max(0,min(255,r_o));
+
+            output.at<Vec3b>(y,x) = pixels1;
+            blue=0;
+            green=0;
+            red=0;
+        }
+    }
+
+}
+void Clahe(int& ClipLimit,Mat& image,Mat& Clahe_out)
+{
+    Clahe_out = Mat::zeros(image.size(),image.type());
+    int hist_r[8][8][256],hist_g[8][8][256],hist_b[8][8][256];
+    int lut_r[8][8][256],lut_g[8][8][256],lut_b[8][8][256];
+    int excess_r[8],excess_g[8],excess_b[8];
+    int cdf_r[8][256],cdf_g[8][256],cdf_b[8][256];
+    int redis_r=0,redis_g=0,redis_b=0;
+    int tile_h = image.rows/8;
+    int tile_w = image.cols/8;
+
+    memset(hist_r,0,sizeof(hist_r));
+    memset(hist_g,0,sizeof(hist_g));
+    memset(hist_b,0,sizeof(hist_b));
+    memset(lut_r,0,sizeof(lut_r));
+    memset(lut_g,0,sizeof(lut_g));
+    memset(lut_b,0,sizeof(lut_b));
+
+    for(int i =0; i<8; i++)
+    {
+        memset(excess_r,0,sizeof(excess_r));
+        memset(excess_g,0,sizeof(excess_g));
+        memset(excess_b,0,sizeof(excess_b));
+        memset(cdf_r,0,sizeof(cdf_r));
+        memset(cdf_g,0,sizeof(cdf_g));
+        memset(cdf_b,0,sizeof(cdf_b));
+
+        int a = tile_h*i;
+
+        for(int j=0; j<8; j++)
+        {
+            for(int y=0; y<tile_h; y++)
+            {
+                for(int x=0; x<tile_w; x++)
+                {
+                    Vec3b pixels = image.at<Vec3b>(y+a,x+(tile_w*j));
+                    unsigned char B = pixels[0];
+                    unsigned char G = pixels[1];
+                    unsigned char R = pixels[2];
+
+                    hist_b[i][j][B] = hist_b[i][j][B]+1;
+                    hist_g[i][j][G] = hist_g[i][j][G]+1;
+                    hist_r[i][j][R] = hist_r[i][j][R]+1;
+                }
+            }
+        }
+        for(int j=0; j<8; j++)
+        {
+            for(int m=0; m<256; m++)
+            {
+                if(hist_b[i][j][m]>ClipLimit)
+                {
+                    excess_b[j]+=(hist_b[i][j][m]-ClipLimit);
+                    hist_b[i][j][m] = ClipLimit;
+                }
+                if(hist_g[i][j][m]>ClipLimit)
+                {
+                    excess_g[j]+=(hist_g[i][j][m]-ClipLimit);
+                    hist_g[i][j][m] = ClipLimit;
+                }
+                if(hist_r[i][j][m]>ClipLimit)
+                {
+                    excess_r[j]+=(hist_r[i][j][m]-ClipLimit);
+                    hist_r[i][j][m] = ClipLimit;
+                }
+            }
+        }
+        for(int j=0; j<8; j++)
+        {
+            redis_b = excess_b[j]/256;
+            redis_g = excess_g[j]/256;
+            redis_r = excess_r[j]/256;
+            for(int m=0; m<256; m++)
+            {
+                hist_b[i][j][m] += redis_b;
+                hist_g[i][j][m] += redis_g;
+                hist_r[i][j][m] += redis_r;
+            }
+            redis_b = 0;
+            redis_g = 0;
+            redis_r = 0;
+        }
+        for(int j=0; j<8; j++)
+        {
+            cdf_b[j][0] = hist_b[i][j][0];
+            cdf_g[j][0] = hist_g[i][j][0];
+            cdf_r[j][0] = hist_r[i][j][0];
+            for(int m =1; m<256; m++)
+            {
+                cdf_b[j][m] =cdf_b[j][m-1] +  hist_b[i][j][m];
+                cdf_g[j][m] =cdf_g[j][m-1] +  hist_g[i][j][m];
+                cdf_r[j][m] =cdf_r[j][m-1] +  hist_r[i][j][m];
+            }
+        }
+        for(int j=0; j<8; j++)
+        {
+            for(int m=0; m<256; m++)
+            {
+                lut_b[i][j][m] = (cdf_b[j][m] * 255)/(tile_h*tile_w);
+                lut_g[i][j][m] = (cdf_g[j][m] * 255)/(tile_h*tile_w);
+                lut_r[i][j][m] = (cdf_r[j][m] * 255)/(tile_h*tile_w);
+
+            }
+        }
+    }
+    for(int y=0; y<image.rows; y++)
+    {
+        for(int x=0; x<image.cols; x++)
+        {
+            Vec3b pixels = image.at<Vec3b>(y,x);
+            unsigned char B = pixels[0];
+            unsigned char G = pixels[1];
+            unsigned char R = pixels[2];
+
+            int tx = x/tile_w;
+            int ty = y/tile_h;
+
+            int tx1 = (tx + 1 < 8) ? tx+1:tx;
+            int ty1 = (ty + 1 < 8) ? ty+1:ty;
+
+            int wx = ((x%tile_w)*255)/tile_w;
+            int wy = ((y%tile_h)*255)/tile_h;
+
+            int p1b = lut_b[ty][tx][B];
+            int p2b = lut_b[ty][tx1][B];
+            int p3b = lut_b[ty1][tx][B];
+            int p4b = lut_b[ty1][tx1][B];
+
+            int p1g = lut_g[ty][tx][G];
+            int p2g = lut_g[ty][tx1][G];
+            int p3g = lut_g[ty1][tx][G];
+            int p4g = lut_g[ty1][tx1][G];
+
+            int p1r = lut_r[ty][tx][R];
+            int p2r = lut_r[ty][tx1][R];
+            int p3r = lut_r[ty1][tx][R];
+            int p4r = lut_r[ty1][tx1][R];
+
+            int outb = ((255-wx)*(255-wy)*p1b + wx*(255-wy)*p2b + (255-wx)*wy*p3b + wx*wy*p4b)>>16;
+            int outg = ((255-wx)*(255-wy)*p1g + wx*(255-wy)*p2g + (255-wx)*wy*p3g + wx*wy*p4g)>>16;
+            int outr = ((255-wx)*(255-wy)*p1r + wx*(255-wy)*p2r + (255-wx)*wy*p3r + wx*wy*p4r)>>16;
+
+            pixels[0] = outb;
+            pixels[1] = outg;
+            pixels[2] = outr;
+
+            Clahe_out.at<Vec3b>(y,x) = pixels;
+        }
+    }
+}
+
+void onParameterchange(int,void* userdata)
+{
+    Parameters* data =(Parameters*)userdata;
+    data->cliplimit = getTrackbarPos("Clip","Parameters");
+    data->val = getTrackbarPos("Gamma","Parameters");
+    data->st = getTrackbarPos("Strength","Parameters");
+    data->c = getTrackbarPos("Center","Parameters");
+    data->level = getTrackbarPos("Sharp","Parameters");
+    gamma_lut(data->val,data->input,data->output);
+    Clahe(data->cliplimit,data->output,data->Clahe_output);
+    Sigmoid_TM(data->st,data->c,data->Clahe_output,data->Sig_output);
+    unmask_sharp(data->level,data->Sig_output,data->final_output);
+//    WBC(data->Sig_output,data->final_output);
+    imshow("Output",data->final_output);
+
+}
+
+//// Using the keyboard to change the value of variables(cliplimit,gamma etc)
+//int main()
+//{
+//    int val=5,st=8,c=5,limit=50;
+//
+//    Mat input = imread("image.bmp");
+//    Mat output = Mat::zeros(input.size(),input.type());
+//
+//    Mat clahe_output = Mat::zeros(input.size(),input.type());
+//    Mat Sigmoid_output = Mat::zeros(input.size(),input.type());
+//    Mat WBC_output = Mat::zeros(input.size(),input.type());
+//
+//
+//    if(input.empty())
+//    {
+//        cout<<"Invalid Input Image";
+//        return -1;
+//    }
+//    while(true)
+//    {
+//        char key = waitKey(0);
+//
+//        if(key=='q')
+//            break;
+//
+//        else if(key=='+')
+//            limit = min(250,limit+10);
+//
+//        else if(key=='-')
+//            limit = max(0,limit-10);
+//
+//        else if(key=='i')
+//            val = min(10,val+1);
+//
+//        else if(key=='k')
+//            val = max(0,val-1);
+//
+//        else if(key=='u')
+//            st = min(15,st+1);
+//
+//        else if(key=='j')
+//            st = max(0,st-1);
+//
+//        else if(key=='o')
+//            c = min(10,c+1);
+//
+//        else if(key=='l')
+//            c = max(0,c-1);
+//
+//        cout<<"Climit = "<<limit<<endl;
+//        cout<<"Val = "<<val<<endl;
+//        cout<<"St = "<<st<<endl;
+//        cout<<"C = "<<c<<endl;
+//
+//        gamma_lut(val,input,output);
+//        Clahe(limit,output,clahe_output);
+//        Sigmoid_TM(st,c,clahe_output,Sigmoid_output);
+//        WBC(Sigmoid_output,WBC_output);
+//
+//        imshow("input",input);
+//        imshow("final_output",WBC_output);
+//    }
+//    imwrite("gamma_output.jpg",output);
+//    imwrite("clahe_output.jpg",clahe_output);
+//    imwrite("final_output.jpg",WBC_output);
+//    return 0;
+//}
+
+// Using the OpenCV Trackbar to change the value of variables(cliplimit,gamma etc)
+int main()
+{
+    Parameters data;
+    int val=5,st=8,c=5,limit=50,level=50;
+    int ID=0;
+    int count_in=0;
+    Mat frame;
+    int count = 0;
+    cout<< "Enter the video file: ";
+//    string input_file = "C:\\Users\\Diwakar\\Documents\\New folder\\Image_Processing\\video\\Emotions-30Sec.mp4";
+    string input_file;
+    getline(cin,input_file);
+    cout << "Enter the frame number: ";
+    cin >> count_in;
+
+    fs::path input_path(input_file);
+
+    string output_file = input_path.stem().string() + "_HDR_Merged" + input_path.extension().string();
+//    fs::path output_path = input_path.parent_path() / (output_file);
+    fs::path output_path = "video/" + (output_file);
+
+    // Reading the input video file to extract audio"
+//    string command_in ="ffmpeg -y "
+//                       "-i \"" + input_path.string() + "\" "
+//                       "-vn "
+//                       "-acodec copy "
+//                       "audio.aac";
+
+     string command_in ="ffmpeg -y "
+                       "-i \"" + input_path.string() + "\" "
+                       "-vn "
+                       "-c:a aac "
+                       "-b:a 192k "
+                       "audio.aac";
+
+    system(command_in.c_str());
+
+    //Creating a folder name, if not there
+    string folder="setting";
+    if(!fs::exists(folder))
+        fs::create_directory(folder);
+
+    folder="video";
+    if(!fs::exists(folder))
+        fs::create_directory(folder);
+
+    folder = "image";
+    if(!fs::exists(folder))
+        fs::create_directory(folder);
+    //--------------------------------------------------
+
+    // Reading the setting.yml file for default the parameters value
+    // IF the program is run for first time there will be no setting.yml file ,but there will be no issue
+    FileStorage fsRead("setting/setting.yml",FileStorage::READ);
+    if(fsRead.isOpened())
+    {
+        fsRead["limit"]>>limit;
+        fsRead["gamma"]>>val;
+        fsRead["strength"]>>st;
+        fsRead["center"]>>c;
+        fsRead["sharp"]>>level;
+        fsRead.release();
+    }
+    VideoCapture vid(input_file);
+    if(!vid.isOpened())
+    {
+        cout<<"Video file not found"<<endl;
+        return -1;
+    }
+    int frame_width = vid.get(CAP_PROP_FRAME_WIDTH);
+    int frame_height = vid.get(CAP_PROP_FRAME_HEIGHT);
+    double fps = vid.get(CAP_PROP_FPS);
+
+    VideoWriter writer("video/output.mp4",VideoWriter::fourcc('a','v','c','1'),fps,Size(frame_width,frame_height));
+
+    if(!writer.isOpened())
+        return -1;
+
+    while(vid.read(frame))
+    {
+        ID++;
+        if(ID==count_in)
+        {
+            break;
+        }
+    }
+
+    data.input = frame;
+//    Mat output = Mat::zeros(input.size(),input.type());
+//
+//    Mat clahe_output = Mat::zeros(input.size(),input.type());
+//    Mat Sigmoid_output = Mat::zeros(input.size(),input.type());
+//    Mat WBC_output = Mat::zeros(input.size(),input.type());
+
+
+    if(data.input.empty())
+    {
+        cout<<"Invalid Input Image";
+        return -1;
+    }
+    //Creating a window for track bar
+    namedWindow("Parameters",WINDOW_AUTOSIZE);
+    // Creating a trackbar for each paramters in a single window
+    createTrackbar("Clip","Parameters",&limit,250,onParameterchange,&data);
+    createTrackbar("Gamma","Parameters",&val,20,onParameterchange,&data);
+    createTrackbar("Strength","Parameters",&st,15,onParameterchange,&data);
+    createTrackbar("Center","Parameters",&c,10,onParameterchange,&data);
+    createTrackbar("Sharp","Parameters",&level,100,onParameterchange,&data);
+
+    //Callback function that will run the code unless terminated
+    onParameterchange(0,&data);
+    waitKey(0);
+    destroyAllWindows();
+
+    imwrite("image/gamma_output.jpg",data.output);
+    imwrite("image/clahe_output.jpg",data.Clahe_output);
+    imwrite("image/Sig_output.jpg",data.Sig_output);
+    imwrite("image/Final_output.jpg",data.final_output);
+
+    // To restart the video frame reading from 0
+    vid.set(CAP_PROP_POS_FRAMES,0);
+
+    while(vid.read(frame))
+    {
+        data.input = frame;
+        if(data.input.empty())
+        {
+            cout << "No input image for reading for HDR processing side" << endl;
+            return -1;
+        }
+        gamma_lut(data.val,data.input,data.output);
+        Clahe(data.cliplimit,data.output,data.Clahe_output);
+        Sigmoid_TM(data.st,data.c,data.Clahe_output,data.Sig_output);
+        unmask_sharp(data.level,data.Sig_output,data.final_output);
+//        WBC(data.Sig_output,data.final_output);
+        writer.write(data.final_output);
+        count++;
+        cout <<count<<endl;
+//        imshow("Video",data.Sig_output);
+//        if(waitKey(30)==27) // esc button
+//            break;
+    }
+    // Releasing the input and output video to complete them
+    vid.release();
+    writer.release();
+
+    // Command for reading the HDR output video and audio.acc and then merge them together
+    string command =
+        "ffmpeg -y "
+        "-i video/output.mp4 "
+        "-i audio.aac "
+        "-c:v copy "
+        "-c:a copy "
+        "\"" + output_path.string() + "\"";
+    system(command.c_str());
+
+    //Write the new values in setting.yml file for next run default values
+    FileStorage fsWrite("setting/setting.yml",FileStorage::WRITE);
+    fsWrite << "limit" << data.cliplimit;
+    fsWrite << "gamma" << data.val;
+    fsWrite << "strength" << data.st;
+    fsWrite << "center" << data.c;
+    fsWrite << "sharp" << data.level;
+    fsWrite.release();
+    return 0;
+}
